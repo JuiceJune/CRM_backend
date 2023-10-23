ff49488d3388434d14e1d707757659579edb0b80
Oleg <oleg.bortovsky@gmail.com>
2023-10-23 12:14:44 +0300

    Change the database structure. Delete Li collection, delete email provider collection.

---
+++ database/migrations/2023_04_27_210607_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('logo');
+            $table->string('name');
+            $table->foreignId('client_id')->constrained();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
