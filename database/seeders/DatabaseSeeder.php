b212e525658a24bcf67af6180be64e3fa4cf4d7c
Oleg <oleg.bortovsky@gmail.com>
2023-10-27 14:06:19 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,15 +13,13 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleSeeder::class,
-            EmailProviderSeeder::class,
             PositionSeeder::class
         ]);
          \App\Models\User::factory()->create(['name' => "admin", 'email' => "admin@admin.com", 'password' => 'admin',
-             'avatar' => 'users/avatars/default.png', 'role_id' => 1]);
+             'avatar' => 'users/avatars/default.png', 'role_id' => 1, 'position_id' => 2]);
          \App\Models\User::factory(30)->create();
          \App\Models\Client::factory(20)->create();
          \App\Models\Mailbox::factory(30)->create();
-         \App\Models\Linkedin::factory(30)->create();
          \App\Models\Project::factory(10)->create();
     }
 }
