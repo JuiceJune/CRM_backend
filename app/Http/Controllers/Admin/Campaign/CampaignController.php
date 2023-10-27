6d46b4cfb05f2aff8dacb1978b56114cc6bb7d43
Oleg <oleg.bortovsky@gmail.com>
2023-10-27 14:01:51 +0300

    Add Campaign controller for Admin and Api

---
+++ app/Http/Controllers/Admin/Campaign/CampaignController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Campaign;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Campaign\CampaignStoreRequest;
+use App\Http\Requests\Admin\Campaign\CampaignUpdateRequest;
+use App\Models\Mailbox;
+use App\Models\Campaign;
+use App\Models\Project;
+
+class CampaignController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $campaigns = Campaign::all();
+        return view('admin.campaign.index', compact('campaigns'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $mailboxes = Mailbox::all();
+        $projects = Project::all();
+        return view('admin.campaign.create',
+            compact('mailboxes', 'projects'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CampaignStoreRequest $request)
+    {
+        $validated = $request->validated();
+
+        $campaign = Campaign::create($validated);
+
+        return redirect()->route('admin.campaigns.index')->with('success', 'Campaign created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $campaign = Campaign::findOrFail($id);
+        return view('admin.campaign.show', compact('campaign'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $campaign = Campaign::findOrFail($id);
+        $mailboxes = Mailbox::all();
+        $projects = Project::all();
+        return view('admin.campaign.edit', compact('campaign', 'mailboxes', 'projects'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CampaignUpdateRequest $request, string $id)
+    {
+        $validated = $request->validated();
+
+        $campaign = Campaign::findOrFail($id);
+
+        $campaign->update($validated);
+
+        return redirect()->route('admin.campaigns.index')->with('success', 'Campaign updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //TODO delete avatar file
+        $campaign = Campaign::find($id);
+        if ($campaign->delete()) {
+            return redirect()->route('admin.campaigns.index')->with('success', 'Campaign deleted successfully.');
+        } else {
+            return redirect()->route('admin.campaigns.index')->with('error', 'Campaign not deleted.');
+        }
+    }
+}
---
+++ app/Http/Controllers/Api/Campaign/CampaignController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Http\Controllers\Api\Campaign;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Campaign\CampaignStoreRequest;
+use App\Http\Requests\Admin\Campaign\CampaignUpdateRequest;
+use App\Http\Resources\CampaignResource;
+use App\Models\Mailbox;
+use App\Models\Campaign;
+use App\Models\Project;
+use Google\Service\Gmail;
+use Illuminate\Http\Request;
+use PHPUnit\Exception;
+
+class CampaignController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        try {
+            $limit = $request->input('limit', 10);
+            $offset = $request->input('offset', 0);
+
+            $query = Campaign::skip($offset)->take($limit);
+
+            $campaigns = $query->get();
+
+            return response(CampaignResource::collection($campaigns));
+        } catch (Exception $error) {
+            return response($error, 400);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        try {
+            $project_id = $request->input('project_id');
+            if ($project_id) {
+                $projects = Project::find($project_id);
+                $mailboxes = $projects->mailboxes;
+                return response(["mailboxes" => $mailboxes]);
+            } else {
+                $mailboxes = Mailbox::select('id', 'email', 'name')->get();
+                $projects = Project::select('id', 'name')->get();
+                return response(["mailboxes" => $mailboxes, "projects" => $projects]);
+            }
+        } catch (Exception $error) {
+            return response($error, 400);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CampaignStoreRequest $request)
+    {
+        try {
+            $validated = $request->validated();
+            $campaign = Campaign::create($validated);
+            return response($campaign);
+        } catch (Exception $error) {
+            return response($error, 400);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Campaign $campaign)
+    {
+        try {
+            $currentCampaign = new CampaignResource($campaign);
+            return response()->json($currentCampaign);
+        } catch (Exception $error) {
+            return response($error, 400);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Campaign $campaign)
+    {
+        try {
+            $project = $campaign->project;
+            $mailboxes = $project->mailboxes;
+            return response(["campaign" => $campaign, "mailboxes" => $mailboxes]);
+        } catch (Exception $error) {
+            return response($error, 400);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CampaignUpdateRequest $request, Campaign $campaign)
+    {
+        try {
+            $validated = $request->validated();
+            $campaign->update($validated);
+            return response('Campaign successfully updated');
+        } catch (Exception $error) {
+            return response($error, 400);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+            $campaign = Campaign::find($id);
+            $campaign->delete();
+            return response('Campaign deleted successfully');
+        } catch (Exception $error) {
+            return response($error, 400);
+        }
+    }
+
+    /**
+     * Send test email.
+     */
+    public function sendTestEmail(Request $request)
+    {
+        try {
+            $mailbox_id = $request->input('mailbox_id');
+            $message = $request->input('message');
+            $subject = $request->input('subject');
+            $test_email = $request->input('test_email');
+            $mailbox = Mailbox::find($mailbox_id);
+            if ($mailbox) {
+                $client = (new \App\Http\Controllers\Api\Google\GoogleController)->getGoogleClient($mailbox["token"]);
+                $sender = $mailbox['name'];
+                $recipient = $test_email; // Адреса отримувача
+                $messageText = $message;
+                $service = new Gmail($client);
+                $message = (new \App\Http\Controllers\Api\Google\GoogleController)->createMessage($sender, $recipient, $subject, $messageText);
+                $response = $service->users_messages->send('me', $message);
+                return response($response);
+            } else {
+                return response('Mailbox not found');
+            }
+        } catch (Exception $error) {
+            return response($error, 400);
+        }
+    }
+}
