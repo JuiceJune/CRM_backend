0d1887b6fa8a1588bea17e81c041ba0443a0c74d
Oleg <oleg.bortovsky@gmail.com>
2023-10-27 14:03:56 +0300

    Add Campaign update request

---
+++ app/Http/Requests/Admin/Campaign/CampaignUpdateRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\Admin\Campaign;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CampaignUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'mailbox_id' => 'required|integer|exists:mailboxes,id',
+            'subject' => 'required|string',
+            'message' => 'required',
+        ];
+    }
+
+    /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'Name is required!',
+            'name.string' => 'Name should be a string!',
+            'name.max' => 'Name should be not longer than 50 chars!',
+        ];
+    }
+}
