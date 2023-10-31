9a253ccd5dbbd3c5529e1474bcff7dbd2bbe078d
Oleg <oleg.bortovsky@gmail.com>
2023-10-31 19:17:50 +0200

    Add to Campaign Model new fields (status, period and sending_time_json)0

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -14,7 +14,28 @@ class Campaign extends Model
         'mailbox_id',
         'project_id',
         'subject',
-        'message'
+        'message',
+        'sending_time_json',
+        'status',
+        'period'
+    ];
+
+    protected $casts = [
+        'sending_time_json' => 'json'
+    ];
+
+    protected $attributes = [
+        'period' => 60,
+        'status' => 'stopped',
+        'sending_time_json' => '{
+        "Mon": [true, "08:00", "15:00"],
+        "Tues": [true, "08:00", "15:00"],
+        "Wed": [true, "08:00", "15:00"],
+        "Thurs": [true, "08:00", "15:00"],
+        "Fri": [true, "08:00", "15:00"],
+        "Sat": [false, "08:00", "15:00"],
+        "Sun": [false, "08:00", "15:00"]
+    }',
     ];
 
     public function mailbox() {
