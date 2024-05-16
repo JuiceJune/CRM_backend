4365d36ce2239436de7c2a63ff42faae95c8ee42
Oleg <oleg.bortovsky@gmail.com>
2024-05-16 17:25:46 +0300

    Add creator field to Projects.

--- database/migrations/2024_05_16_171002_add_creator_column_to_projects_table.php
+++ database/migrations/2024_05_16_171002_add_creator_column_to_projects_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->foreignId('creator_id')->constrained();
+            $table->foreignId('creator_id')->constrained('users');
         });
     }
 
